94f0276c792a72ae75457c7a34edf5a153e7d3e4
Emil Skogsund <skogix@gmail.com>
2020-07-09 02:01:54 +0200

    000

--- ECS/World/World.cs
+++ ECS/World/World.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 
 namespace ECS
 {
@@ -7,12 +8,46 @@ namespace ECS
         protected List<EntityData> Entities;
         protected int EntitiesCount;
 
+        private protected readonly Dictionary<int, List<Filter>> FilterByIncludedComponents;
+        private protected readonly Dictionary<int, List<Filter>> FilterByExcludedComponents;
+
         public List<IComponentPool> ComponentPools;
 
         public World()
         {
             Entities = new List<EntityData>();
             ComponentPools = new List<IComponentPool>();
+            FilterByIncludedComponents = new Dictionary<int, List<Filter>>();
+            FilterByExcludedComponents = new Dictionary<int, List<Filter>>();
+        }
+
+        /// <summary>
+        /// Uppdaterar filters
+        /// Minus för remove, plus för add
+        /// Skicka in target
+        /// </summary>
+        protected internal void UpdateFilters(int typeId, in Entity entity)
+        {
+            List<Filter> filters;
+            
+            if (typeId < 0)
+            {
+                // ta bort component component
+                if (FilterByIncludedComponents.TryGetValue(-typeId, out filters))
+                    foreach (var filter in filters)
+                        filter.OnAddEntity(entity);
+                if (FilterByExcludedComponents.TryGetValue(-typeId, out filters))
+                    foreach (var filter in filters)
+                        filter.OnRemoveEntity(entity);
+            } else {
+                // lägg till component
+                if (FilterByIncludedComponents.TryGetValue(typeId, out filters))
+                    foreach (var filter in filters)
+                        filter.OnAddEntity(entity);
+                if (FilterByExcludedComponents.TryGetValue(typeId, out filters))
+                    foreach (var filter in filters)
+                        filter.OnRemoveEntity(entity);
+            }
         }
     }
 }
